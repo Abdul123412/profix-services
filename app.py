26c64b97030e11f2ce20fc2c9e9ce2a5437d977e
Abdul123412 <mohammedabdulmujib14@gmail.com>
2026-05-31 14:24:21 +0100

    Update app.py

--- app.py
+++ app.py
@@ -3,8 +3,8 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 workers = [
-    {"name": "Redeemer Ahiable", "job": "Plumber", "contact": "0592110378", "rating": "4.8"},
-    {"name": "Abdul Mujib", "job": "Electrician", "contact": "0502021889", "rating": "4.9"},
+    {"name": "Redeemer Atsu", "job": "Plumber", "contact": "0544304526", "rating": "4.8"},
+    {"name": "Abdul Mujib", "job": "Electrician", "contact": "0506351995", "rating": "4.9"},
     {"name": "Master Francis", "job": "Carpenter", "contact": "0240278488", "rating": "4.7"},
     {"name": "Abdul Hamid", "job": "Painter", "contact": "0543333313", "rating": "4.6"}
 ]
