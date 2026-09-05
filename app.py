535236509dc7a839ff6e39989703b670e0fc198c
Abdul123412 <mohammedabdulmujib14@gmail.com>
2026-09-05 22:27:11 +0100

    Update app.py
    Fixed grouped accordion

--- app.py
+++ app.py
@@ -1,17 +1,21 @@
 from flask import Flask, render_template
+from collections import defaultdict
 
 app = Flask(__name__)
 
 workers = [
     {"name": "Redeemer Atsu", "job": "Plumber", "contact": "0544304526", "rating": "4.8"},
-    {"name": "Abdul Mujib", "job": "Electrician", "contact": "0506351995", "rating": "4.9"},
-    {"name": "Master Francis", "job": "Carpenter", "contact": "0240278488", "rating": "4.7"},
-    {"name": "Abdul Hamid", "job": "Painter", "contact": "0543333313", "rating": "4.6"}
+    {"name": "Abdul Mujib", "job": "Electrician", "contact": "0506351995", "rating": "5.0"},
+    {"name": "Master Francis", "job": "Carpenter", "contact": "0240278488", "rating": "4.9"},   
+    {"name": "Abdul Hamid", "job": "Painter", "contact": "0543333313", "rating": "4.6"},  
 ]
 
 @app.route('/')
 def home():
-    return render_template('index.html', workers=workers)
+    grouped = defaultdict(list)
+    for w in workers:
+        grouped[w['job']].append(w)
+    return render_template('index.html', jobs=grouped)
 
 if __name__ == '__main__':
     app.run(debug=True)
